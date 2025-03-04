d53051aedf56cf276f7b374f2af7731b9758dcbf
alifbinabdulqayyum <alifbinabdulqayyum@tamu.edu>
2025-03-04 14:25:10 -0600

    Updated bash scripts

--- scripts/opt/opt-chem-viable.sh
+++ scripts/opt/opt-chem-viable.sh
@@ -1,5 +1,5 @@
 gpu="--gpu"  # change to "" if no GPU is to be used
-seed=730007773
+seed_array=( 7300077731 7300077732 7300077733 7300077734 7300077735 )
 
 pathway_model="viable"
 parp_model_path="updated_pXC50_predictor/PARP1_CGUAgg_2022-06_fingerprint_graphconv_model_4f296899-1e4f-4d08-a7c5-47ef64d7fec3.tar.gz"
@@ -14,32 +14,46 @@ r=50
 weight_type="rank"
 lso_strategy="opt"
 
-k=6
+k_expt=( 6 5 4 3 )
+
+expt_index=0  # Track experiments
 
 echo "Property to optimize: therapeutic score"
 echo "Pathway Model to be used: $pathway_model"
 
 # Run command
-python weighted_retraining/opt_scripts/opt_chem.py \
-    --batch_size=32 \
-    --seed="$seed" $gpu \
-    --query_budget="$query_budget" \
-    --retraining_frequency="$r" \
-    --result_root="${root_dir}/${weight_type}/k_${k}/r_${r}/seed${seed}" \
-    --pretrained_model_file="$start_model" \
-    --lso_strategy="$lso_strategy" \
-    --train_path="data/chem/orig_model/tensors_train" \
-    --val_path="data/chem/orig_model/tensors_val" \
-    --vocab_file="data/chem/orig_model/vocab.txt" \
-    --property_file="data/chem/orig_model/therapeutic_score_$pathway_model.pkl" \
-    --property='therapeutic_score' \
-    --n_retrain_epochs="$n_retrain_epochs" \
-    --n_init_retrain_epochs="$n_init_retrain_epochs" \
-    --n_best_points=2000 --n_rand_points=8000 \
-    --n_inducing_points=500 \
-    --invalid_score=0 \
-    --weight_type="$weight_type" --rank_weight_k="1e-$k" \
-    --samples_per_model=1000 \
-    --pathway_model="$pathway_model" \
-    --parp_model_path="$parp_model_path" \
-    --bngl_model_path="$bngl_model_path"
+for seed in "${seed_array[@]}"; do
+    for ((i=0;i<${#k_expt[@]};++i)); do
+        # Increment experiment index
+        expt_index=$((expt_index+1))
+
+        # Echo info of task to be executed
+        k="${k_expt[$i]}"
+        echo "k=${k} seed=${seed}"
+
+        python weighted_retraining/opt_scripts/opt_chem.py \
+            --batch_size=32 \
+            --seed="$seed" $gpu \
+            --query_budget="$query_budget" \
+            --retraining_frequency="$r" \
+            --result_root="${root_dir}/${weight_type}/k_${k}/r_${r}/seed${seed}" \
+            --pretrained_model_file="$start_model" \
+            --lso_strategy="$lso_strategy" \
+            --train_path="data/chem/orig_model/tensors_train" \
+            --val_path="data/chem/orig_model/tensors_val" \
+            --vocab_file="data/chem/orig_model/vocab.txt" \
+            --property_file="data/chem/orig_model/therapeutic_score_$pathway_model.pkl" \
+            --property='therapeutic_score' \
+            --n_retrain_epochs="$n_retrain_epochs" \
+            --n_init_retrain_epochs="$n_init_retrain_epochs" \
+            --n_best_points=2000 --n_rand_points=8000 \
+            --n_inducing_points=500 \
+            --invalid_score=0 \
+            --weight_type="$weight_type" --rank_weight_k="1e-$k" \
+            --samples_per_model=1000 \
+            --pathway_model="$pathway_model" \
+            --parp_model_path="$parp_model_path" \
+            --bngl_model_path="$bngl_model_path"
+
+    done
+done
